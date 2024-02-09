c1be276d68766cf0da4b2e1ba2550e95d95ce590
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2024-02-09 18:35:36 +0000

    dev-1.5

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """
 Author: Kelvin Gooding
 Created: 2024-01-18
-Updated: 2024-01-31
-Version: dev-1.4
+Updated: 2024-02-09
+Version: dev-1.5
 """
 
 #!/usr/bin/python3
@@ -11,6 +11,7 @@ Version: dev-1.4
 
 from flask import Flask, render_template, request, flash
 import sqlite3
+import os
 
 # SQLite3 Variables
 
@@ -20,6 +21,8 @@ c = connection.cursor()
 # Flask Variables
 
 app = Flask(__name__)
+app = Flask(__name__)
+app.secret_key = os.urandom(26)
 
 @app.route("/" , methods=['POST', 'GET'])
 def index():
@@ -112,6 +115,24 @@ def index():
             ns_games.append([i[0].upper(), i[1], i[5], i[9], int(i[7])])
         
         return render_template("index.html", ps5_games=ps5_games, ps5_count=len(ps5_games), ps4_games=ps4_games, ps4_count=len(ps4_games), ns_games=ns_games, ns_count=len(ns_games))
+
+@app.route("/new_entry", methods=["POST", 'GET'])
+def new_entry():
+
+    status = ['Playing', 'Completed', 'Backlog', 'Wishlist',]
+    
+    platform = ['PS5', 'PS4', 'Nintendo Switch',]
+
+    format = ['Physical', 'Digital']
+
+    if request.method == "POST":
+        c.execute('INSERT INTO gamelist VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (request.form.get("name"), request.form.get("release_date"), request.form.get("players"), request.form.get("genre"), request.form.get("status"), request.form.get("platform"), request.form.get("format"), request.form.get("rating"), request.form.get("link"), request.form.get("cover_image"),))
+        connection.commit()
+
+        flash("A new entry has now been added!")
+        return render_template("new_entry.html", status=status, platform=platform, format=format)
+    
+    return render_template("new_entry.html", status=status, platform=platform, format=format)
 
 if __name__ == "__main__":
     app.debug = True
